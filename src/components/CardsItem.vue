f27f8166ff9a4b7065fab0ce9125683aee958353
unknown <6mukomel9@gmail.com>
2023-05-23 23:58:14 +0300

    исправлена главн сттраница

--- src/components/CardsItem.vue
+++ src/components/CardsItem.vue
@@ -30,33 +30,3 @@ export default {
 
 </script>
 
-
-
-<!-- <template>
-    <div class="mr-4 ml-4 mb-4 w-[240px] p-4 border-2 border-gray-900 rounded-md  shadow-md flex flex-col justify-between"
-            v-for="card in cards"
-            :key="card.id"
-            :title="card.name"
-    >
-        <img :src="require('../assets/'+ card.img)" alt="" class="object-cover object-center w-full rounded-md  dark:bg-gray-500">
-
-        <div class="mt-6 mb-2">
-            <span class="block text-xs font-medium tracking-widest uppercase dark:text-violet-400">{{ card.name }}</span>
-        </div>
-        <p class="dark:text-gray-100">{{ card.price }}</p>
-    </div>
-</template>
-
-
-
-<script>
-export default {
-    name: "CardsItem",
-    props: {
-        cards: {
-            type: Array
-        },
-  }
-  }
-
-</script> -->
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -5,19 +5,46 @@
         <CarouselUniversal v-bind:photos="state.photosCarousel"/>
         
         <p class="text-[24px] ml-6 sm:ml-20 mb-6 ">Возможно, вам понравится</p>
-        <div class="mb-20 flex justify-start flex-wrap w-250 px-6 sm:px-[60px] md:px-[100px] lg:px-[90px] xl:px-[120px] 2xl:p-0 2xl:w-[1380px] mx-auto ">
-            <CardsItem
-                v-bind:cards="state.CardsYouEnjoy"
-            />
-        </div>
-    
-    
-        <p class="text-[24px] ml-6 sm:ml-20 mb-6">Вы ранее смотрели</p>
+        
         <div class="mb-20 flex justify-start flex-wrap w-250 px-4 sm:px-[60px] md:px-[100px] lg:px-[90px] xl:px-[120px] 2xl:p-0 2xl:w-[1380px] mx-auto ">
-            <CardsItem
-                v-bind:cards="state.CardsYouEnjoy"
-            />
+            <div class="mr-4 ml-4 mb-4 w-[240px] p-4 border-2 border-gray-900 rounded-md  shadow-md flex flex-col justify-between"
+                    v-for="card in state.CardsYouEnjoy"
+                    :key="card.id"
+                    :title="card.name"
+            >
+                <img :src="require('../assets/'+ card.img)" alt="" class="object-cover object-center w-full rounded-md  dark:bg-gray-500">
+
+                <div class="mt-6 mb-2">
+                    <span class="block text-xs font-medium tracking-widest uppercase dark:text-violet-400">{{ card.name }}</span>
+                </div>
+                <p class="dark:text-gray-100">{{ card.price }}</p>
+            </div>
         </div>
+        
+
+        
+    
+    
+        <p class="text-[24px] ml-6 sm:ml-20 mb-6 ">Вы ранее смотрели:</p>
+        
+        <div class="mb-20 flex justify-start flex-wrap w-250 px-4 sm:px-[60px] md:px-[100px] lg:px-[90px] xl:px-[120px] 2xl:p-0 2xl:w-[1380px] mx-auto ">
+            <div class="mr-4 ml-4 mb-4 w-[240px] p-4 border-2 border-gray-900 rounded-md  shadow-md flex flex-col justify-between"
+                    v-for="card in state.CardsYouEnjoy"
+                    :key="card.id"
+                    :title="card.name"
+            >
+                <img :src="require('../assets/'+ card.img)" alt="" class="object-cover object-center w-full rounded-md  dark:bg-gray-500">
+
+                <div class="mt-6 mb-2">
+                    <span class="block text-xs font-medium tracking-widest uppercase dark:text-violet-400">{{ card.name }}</span>
+                </div>
+                <p class="dark:text-gray-100">{{ card.price }}</p>
+            </div>
+        </div>
+
+
+
+        
     
     
     </div>
@@ -44,10 +71,8 @@ export default {
             ],
             photosCarousel: ["slider1.png", "slider2.png", "slider1.png"]
         });
-        // const login = () => {
-        // }
+        
         return {
-            //   login,
             state
         };
     }
